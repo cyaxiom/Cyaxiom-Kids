25555b15976b092d4b8e4abd9ec847f3370023ca
etabebe1 <etabebe1@gmail.com>
2025-04-09 17:12:47 -0400

    Updated home component structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+import React from "react"; // Add this import
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
   return (
-    <>
-      <h1 className="bg-blue-500 text-8xl">Spin app.js</h1>
-    </>
+    <div className="flex flex-col">
+      <Navbar />
+
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
